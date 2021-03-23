c49033885007ebd2143d3df9ba062dc5e44be650
Moises Nervis <Nervis@Macs-iMac-2.local>
2021-03-23 14:51:21 -0700

    Added some style on Tuesday

--- src/components/EventCard.jsx
+++ src/components/EventCard.jsx
@@ -1,21 +1,49 @@
 import React, { useState, useCallback, useEffect } from 'react'
 
-const EventCard = (props) => {
+// Stilyng
+import { Typography, AppBar, Button, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container } from '@material-ui/core'
+import AddLocationTwoToneIcon from '@material-ui/icons/AddLocationTwoTone';
+import ShareIcon from '@material-ui/icons/Share';
+import StarIcon from '@material-ui/icons/Star';
+import PinDropIcon from '@material-ui/icons/PinDrop';
+import useStyles from '../styles/styles'
 
+const EventCard = (props) => {
+    const classes = useStyles();
 
     return(
         <>
 
-            <span>{props.event.title}</span>
-            <span>{props.event.date}</span>
-            <span>{props.event.location}</span>
-            <span>{props.event.image}</span>
-            <span>people going: </span>
-            <button>I'm Going</button>
-            
+            <Grid item xs={12} sm={6}>
+             <Card className={classes.card}>
+               <CardMedia className={classes.cardMedia} 
+               image='https://source.unsplash.com/random'
+               title='Image title'
+               />
+               <CardContent className={classes.cardContent}>
+                 <Typography gutterBottom variant='p'>
+                    {props.event.date}
+                 </Typography>
+                 <Typography gutterBottom variant='h4'>
+                     {props.event.title}
+                 </Typography>
+                 <Typography gutterBottom variant='h6'color='primary'>
+                 <PinDropIcon className={classes.icon}/>{props.event.location}
+                 </Typography>
+                 <Typography>
+                   Brief description Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptate, reiciendis!
+                 </Typography>
+               </CardContent>
+                 <CardActions>
 
+                   <Button size='small' color='primary'>I'm Going</Button>
+                   <Button size='small' color='default'>See Event details</Button>
+                   <StarIcon className={classes.icon}/>
+                   <ShareIcon className={classes.icon}/>
 
-
+                 </CardActions>
+             </Card>
+           </Grid>
         </>
     )
 }
